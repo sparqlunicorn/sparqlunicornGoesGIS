a695a96dbb11e46d06948c7e184f7d142a89a053
timo.homburg <timo.homburg@hs-mainz.de>
2020-05-14 18:04:05 +0200

    Updated search dialog

--- searchdialog.py
+++ searchdialog.py
@@ -1,5 +1,7 @@
 
 from qgis.PyQt.QtWidgets import QDialog, QLabel, QLineEdit,QPushButton,QListWidget,QComboBox,QMessageBox,QRadioButton,QListWidgetItem,QTableWidgetItem
+from qgis.PyQt.QtCore import QRegExp
+from qgis.PyQt.QtGui import QRegExpValidator,QValidator
 from SPARQLWrapper import SPARQLWrapper, JSON
 import json
 import requests
@@ -25,8 +27,9 @@ class SearchDialog(QDialog):
         self.interlinkOrEnrich=interlinkOrEnrich
         self.conceptSearchEdit = QLineEdit(self)
         self.conceptSearchEdit.move(110,10)
+        self.conceptSearchEdit.setMinimumSize(180,25)
         conceptSearchLabel = QLabel("Search Concept:",self)
-        conceptSearchLabel.move(0,10)
+        conceptSearchLabel.move(5,10)
         self.findConcept = QRadioButton("Class",self)
         self.findConcept.move(400,15)
         if column!=4:
@@ -45,18 +48,23 @@ class SearchDialog(QDialog):
             if not "File"==triplestore["name"]:
                 self.tripleStoreEdit.addItem(triplestore["name"])
         tripleStoreLabel = QLabel("Triple Store:",self)
-        tripleStoreLabel.move(0,40)
+        tripleStoreLabel.move(5,40)
         searchButton = QPushButton("Search",self)
-        searchButton.move(10,70)
+        searchButton.move(300,10)
         searchButton.clicked.connect(self.getClassesFromLabel)
-        #costumpropertyLabel = QLabel("Define Own URI:",self)
-        #costumpropertyLabel.move(200,75)
-        #costumproperty = QLineEdit(self)
-        #costumproperty.move(300,70)
-        #costumproperty.setMinimumSize(200,25)
-        #costumpropertyButton = QPushButton("Use Own Class/Property",self)
-        #costumpropertyButton.move(500,70)
-        #costumpropertyButton.clicked.connect(self.applyConceptToColumn)
+        costumpropertyLabel = QLabel("Define Own URI:",self)
+        costumpropertyLabel.move(10,75)
+        urlregex = QRegExp("http[s]?://(?:[a-zA-Z#]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
+        urlvalidator = QRegExpValidator(urlregex, self)
+        self.costumproperty = QLineEdit(self)
+        self.costumproperty.move(110,70)
+        self.costumproperty.setMinimumSize(200,25)
+        self.costumproperty.setValidator(urlvalidator)
+        self.costumproperty.textChanged.connect(self.check_state3)
+        self.costumproperty.textChanged.emit(self.costumproperty.text())
+        costumpropertyButton = QPushButton("Use Own Class/Property",self)
+        costumpropertyButton.move(315,70)
+        costumpropertyButton.clicked.connect(self.applyConceptToColumn2)
         searchResultLabel = QLabel("Search Results",self)
         searchResultLabel.move(100,100)
         self.searchResult = QListWidget(self)
@@ -65,7 +73,21 @@ class SearchDialog(QDialog):
         applyButton = QPushButton("Apply",self)
         applyButton.move(150,430)
         applyButton.clicked.connect(self.applyConceptToColumn)
+
+    def check_state3(self):
+        self.check_state(self.costumproperty)
 		
+    def check_state(self,sender):
+        validator = sender.validator()
+        state = validator.validate(sender.text(), 0)[0]
+        if state == QValidator.Acceptable:
+            color = '#c4df9b' # green
+        elif state == QValidator.Intermediate:
+            color = '#fff79a' # yellow
+        else:
+            color = '#f6989d' # red
+        sender.setStyleSheet('QLineEdit { background-color: %s }' % color)
+
     """Returns classes for a given label from a triple store."""
     def getClassesFromLabel(self,comboBox):
         viewlist=[]
@@ -114,13 +136,21 @@ class SearchDialog(QDialog):
                 self.searchResult.addItem(item)
                 i+=1
         return viewlist
-		
-    def applyConceptToColumn(self):
+
+    def applyConceptToColumn2(self):
+        self.applyConceptToColumn(True)
+
+    def applyConceptToColumn(self,costumURI=False):
         print("test")
-        if self.searchResult.count()==0:
-            return           
-        if self.bothOptions==True:	
+        if costumURI:
+            if self.costumproperty.text()=="":
+                return   
+            toinsert=self.costumproperty.text()
+        else:
+            if self.searchResult.count()==0:
+                return   
             toinsert=str(self.searchResult.currentItem().data(1))
+        if self.bothOptions==True:	
             haschanged=False
             for prefix in self.currentprefixes:
                 if self.currentprefixes[prefix] in toinsert:
@@ -131,11 +161,15 @@ class SearchDialog(QDialog):
             else:
                 self.table.insertPlainText("<"+toinsert+">")
         elif self.interlinkOrEnrich==-1:
-            self.table.setText(str(self.searchResult.currentItem().data(1)))
+            self.table.setText(str(toinsert))
         else:
-            item=QTableWidgetItem(self.searchResult.currentItem().text())
-            item.setText(self.searchResult.currentItem().text())
-            item.setData(1,self.searchResult.currentItem().data(1))
+            if costumURI:
+                item=QTableWidgetItem(toinsert)
+                item.setText(toinsert)
+            else:
+                item=QTableWidgetItem(self.searchResult.currentItem().text())
+                item.setText(self.searchResult.currentItem().text())
+            item.setData(1,toinsert)
             if self.interlinkOrEnrich:
                 self.table.setItem(self.currentrow,self.currentcol,item)
             else:
