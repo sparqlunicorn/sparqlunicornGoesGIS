581005a35784edcde46eda4313e512e28dfa9500
situx <situx@users.noreply.github.com>
2020-07-24 17:38:53 +0000

    Deploying to gh-pages from  @ ea74acdddaa301552cbfae73f106cdde716762c5 🚀

--- a00090.js
+++ a00090.js
@@ -1,16 +1,14 @@
 var a00090 =
 [
-    [ "__init__", "a00090.html#a33a5cd496649701da2ab8b134be4bf20", null ],
+    [ "__init__", "a00090.html#a6279e408b45c227bf20f68ee44c7f43b", null ],
     [ "finished", "a00090.html#a5f4d426f44e2bbf0bcca08c50d1b5ee4", null ],
     [ "run", "a00090.html#ad22709b2e67308af35f55680d5a026e0", null ],
-    [ "enrichLayer", "a00090.html#afe27f37e04aafb99a9b48eae8506bc9a", null ],
+    [ "columnmap", "a00090.html#a1c2cf58b17f57abcbd2d8fed269b2b6e", null ],
     [ "exception", "a00090.html#aec2b4fd1f327cbebb003cfc415e8bccb", null ],
     [ "labels", "a00090.html#abc325fc13d4194905c1786e24ee447f0", null ],
     [ "prefixes", "a00090.html#a302e350558edcbbd7b85927b33550548", null ],
     [ "query", "a00090.html#a6c84ba0942252fbf3bc9f20120a12ad1", null ],
     [ "results", "a00090.html#a339bb5524b06e496a6aaa0a97a4a8d21", null ],
-    [ "searchResult", "a00090.html#a0e243f776f08c4dd077c3bca68a755a2", null ],
-    [ "searchTerm", "a00090.html#a2e03a9ba37f49da2200795b1080ef4f4", null ],
     [ "sortedatt", "a00090.html#a48ad5f81f31645c924cfff541c5add36", null ],
     [ "triplestoreurl", "a00090.html#a9b8734be629bbc1f64723d90a67861ab", null ],
     [ "urilist", "a00090.html#ac03da770856c2a61cdcfa3124a6d17e8", null ]
