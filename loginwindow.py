203ae019af964209caf3151e7a65991a476f623e
timo.homburg <timo.homburg@hs-mainz.de>
2020-08-01 22:36:29 +0200

    Oauth test

---
+++ loginwindow.py
@@ -0,0 +1,29 @@
+import sys
+from urllib.parse import urlencode, parse_qs
+from PyQt5 import QtCore
+from PyQt5.QtWebEngineCore import QWebEngineUrlRequestInterceptor
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication
+
+class LoginWindow(QWebEngineView):
+    logged_in = QtCore.pyqtSignal(['QString'])
+
+    def __init__(self, app, authurl,interceptor):
+        super(LoginWindow, self).__init__()
+        self.nam = self.page()
+        self.app = app
+        self.setUrl(QUrl(authurl))
+        self.show()
+        self.loadFinished.connect(self._loadFinished)
+        self.interceptor = interceptor
+        self.page().profile().setRequestInterceptor(interceptor)
+        sys.exit(app.exec_())
+
+    def _loadFinished(self, result):
+        self.page().toHtml(self.callable)
+
+    def callable(self, data):
+        self.html = data
+		
+
---
+++ oauth.py
@@ -0,0 +1,115 @@
+import sys
+from urllib.parse import urlencode, parse_qs
+import requests
+from PyQt5 import QtCore
+from PyQt5.QtWebEngineCore import QWebEngineUrlRequestInterceptor
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtWidgets import QApplication
+
+class RequestInterceptor(QWebEngineUrlRequestInterceptor):
+
+    oauthcode=None
+
+    def __init__(self, app):
+        super(RequestInterceptor, self).__init__()
+        self.app = app
+
+    def interceptRequest(self, info):
+        if RedirectUrl == (info.requestUrl().host()+info.requestUrl().path()):
+            params = parse_qs(info.requestUrl().query())
+            if 'code' in params.keys():
+                print('OAuth code is {code}'.format(code=params['code']))
+                self.oauthcode=params['code']
+
+"""
+class GoogleRequestInterceptor(RequestInterceptor):
+	
+	def __init__(self, app):
+        super(RequestInterceptor, self).__init__()
+        self.app = app
+		
+    def interceptRequest(self, info):
+        if RedirectUrl == (info.requestUrl().host()+info.requestUrl().path()):
+            params = parse_qs(info.requestUrl().query())
+            if 'code' in params.keys():
+                print('OAuth code is {code}'.format(code=params['code']))
+                self.oauthcode=params['code']
+				
+    def getUserInformation(self):
+        r=requests.get("https://oauth2.googleapis.com/tokeninfo?id_token="+self.authcode)
+		res = r.json()
+        user=UserMetaData()
+ 
+class ORCIDRequestInterceptor(RequestInterceptor):
+	
+	def __init__(self, app):
+        super(RequestInterceptor, self).__init__()
+        self.app = app
+		
+    def interceptRequest(self, info):
+        if RedirectUrl == (info.requestUrl().host()+info.requestUrl().path()):
+            params = parse_qs(info.requestUrl().query())
+            if 'code' in params.keys():
+                print('OAuth code is {code}'.format(code=params['code']))
+                self.oauthcode=params['code']
+				
+    def getUserInformation(self):
+
+class GithubRequestInterceptor(RequestInterceptor):
+	
+	def __init__(self, app):
+        super(RequestInterceptor, self).__init__()
+        self.app = app
+		
+    def interceptRequest(self, info):
+        if RedirectUrl == (info.requestUrl().host()+info.requestUrl().path()):
+            params = parse_qs(info.requestUrl().query())
+            if 'code' in params.keys():
+                print('OAuth code is {code}'.format(code=params['code']))
+                self.oauthcode=params['code']
+				
+    def getUserInformation(self):
+
+"""
+class UserMetaData:
+
+    username=None
+	 
+    firstname=None
+
+    lastname=None
+	
+    userid=None
+	
+    name=None
+	
+    authenticatedby=None
+	
+    userurl=None
+	
+    
+
+class GitlabRequestInterceptor(RequestInterceptor):
+	
+	def __init__(self, app):
+        super(RequestInterceptor, self).__init__()
+        self.app = app
+		
+    def interceptRequest(self, info):
+        if RedirectUrl == (info.requestUrl().host()+info.requestUrl().path()):
+            params = parse_qs(info.requestUrl().query())
+            if 'code' in params.keys():
+                print('OAuth code is {code}'.format(code=params['code']))
+                self.oauthcode=params['code']
+				
+    def getUserInformation(self,baseurl):
+        r = requests.get(baseurl+"/users?username="+)
+		res = r.json()
+        user=UserMetaData()
+        user.username=res["username"]
+        user.name=res["name"]
+        user.userurl=res["web_url"]
+        user.userid=res["id"]
+        user.authenticatedby="Gitlab ("+baseurl+")"
+		
