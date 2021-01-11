f1cf4d1f6cffff0588d714a9d55429ae8f47bbb1
timo.homburg <timo.homburg@hs-mainz.de>
2021-01-11 13:47:07 +0100

    Fix

--- geoconceptsquerytask.py
+++ geoconceptsquerytask.py
@@ -47,9 +47,9 @@ class GeoConceptsQueryTask(QgsTask):
         QgsMessageLog.logMessage('Started task "{}"'.format(self.description()), MESSAGE_CATEGORY, Qgis.Info)
         if self.proxyHost!=None and self.proxyPort!=None:
             QgsMessageLog.logMessage('Proxy? '+str(self.proxyHost), MESSAGE_CATEGORY, Qgis.Info)
-            proxy = urllib.ProxyHandler({'http': proxyHost})
-            opener = urllib.build_opener(proxy)
-            urllib.install_opener(opener)
+            proxy = urllib.request.ProxyHandler({'http': proxyHost})
+            opener = urllib.request.build_opener(proxy)
+            urllib.request.install_opener(opener)
         sparql = SPARQLWrapper(self.triplestoreurl, agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11")
         print(str(self.query))
         sparql.setQuery(self.query)
