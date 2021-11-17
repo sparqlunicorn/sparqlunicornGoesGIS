55ed28da7cbf4ae965856a67825a86fdf192fbef
situx <situx@users.noreply.github.com>
2021-11-17 00:57:01 +0000

    Deploying to gh-pages from  @ a93a90454dc30ac547ebd7fcaaa4e4eadf8f1b52 🚀

--- a00389.js
+++ a00389.js
@@ -7,5 +7,14 @@ var a00389 =
     [ "invertPrefixes", "a00389.html#af07a1ff1a6064d31434cde2dfe8be93b", null ],
     [ "labelFromURI", "a00389.html#a8d66b18f94f098b37701c05f1a838047", null ],
     [ "loadGraph", "a00389.html#a2ea69b3c9b5e9a5c623f23dd8d123965", null ],
+    [ "classicon", "a00389.html#a48ef9378cd3b1e2cda96ca851adb3e0d", null ],
+    [ "classnode", "a00389.html#a6d4a1839adfb663372bf95fd65b0e039", null ],
+    [ "collectionclassnode", "a00389.html#a670cc0c66359d5042a42639aa6a9e11a", null ],
+    [ "earthinstanceicon", "a00389.html#a31619e6fc29fac22163570db6ae6dbfc", null ],
+    [ "geoclassicon", "a00389.html#a10e149d3486f54292669f30e13cff863", null ],
+    [ "geoclassnode", "a00389.html#abe5ec0a47371449485744620d324f1f3", null ],
+    [ "geoinstancenode", "a00389.html#aecf1a33e8d4dc9134317cdcc36c7eb03", null ],
+    [ "instanceicon", "a00389.html#a7957119a35f294ef57eb7479c0a56953", null ],
+    [ "instancenode", "a00389.html#a5e97724ca0b22642839ab3dfff960c7d", null ],
     [ "supportedLiteralTypes", "a00389.html#a28a4112f838898d008910c4320f0732e", null ]
 ];
