dfc7c1de094097d6c251e5547a8aeaea1d9781f6
situx <situx@users.noreply.github.com>
2021-11-17 22:01:25 +0000

    Deploying to gh-pages from  @ dc165608f1401274b865a8e3b9e0ec8b19fd5ca1 🚀

--- a00373.js
+++ a00373.js
@@ -1,7 +1,8 @@
 var a00373 =
 [
-    [ "__init__", "a00373.html#a3047f94dcb7e376099d2e6d9856cb8fb", null ],
+    [ "__init__", "a00373.html#af69c3a8f180ead6f0a55751c1c11fea7", null ],
     [ "interceptRequest", "a00373.html#aa20a76c1bc9a728ff7aa3dc56ea6bad2", null ],
     [ "app", "a00373.html#afe63fea7be31b0200b496d08bc6b517d", null ],
-    [ "oauthcode", "a00373.html#ac89f05d3eeb1c5c95ecd3fde0ffc1abd", null ]
+    [ "oauthcode", "a00373.html#ac89f05d3eeb1c5c95ecd3fde0ffc1abd", null ],
+    [ "provider", "a00373.html#a7de7ae15cfa7d49adc28e64a859a2d45", null ]
 ];
--- a00381.js
+++ a00381.js
@@ -4,5 +4,6 @@ var a00381 =
     [ "getUserInformation", "a00381.html#aea3d6e87f3e13680be9d249ccdab7854", null ],
     [ "interceptRequest", "a00381.html#aa20a76c1bc9a728ff7aa3dc56ea6bad2", null ],
     [ "app", "a00381.html#afe63fea7be31b0200b496d08bc6b517d", null ],
-    [ "oauthcode", "a00381.html#ac89f05d3eeb1c5c95ecd3fde0ffc1abd", null ]
+    [ "oauthcode", "a00381.html#ac89f05d3eeb1c5c95ecd3fde0ffc1abd", null ],
+    [ "provider", "a00381.html#a7de7ae15cfa7d49adc28e64a859a2d45", null ]
 ];
