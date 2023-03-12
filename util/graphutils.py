024c08f534c0a74a962231c6b32c05f18f3ea18f
situx <timo.homburg@gmx.de>
2023-03-12 02:48:27 +0100

    Detect equiv classes and props for wikibase detection

--- util/graphutils.py
+++ util/graphutils.py
@@ -198,6 +198,34 @@ class GraphUtils:
                 reslist.append(nss["ns"]["value"])
         return reslist
 
+    def detectEquivalentProperties(self,triplestoreurl,credentialUserName,credentialPassword, authmethod,equivalentPropProperty="http://www.w3.org/2002/07/owl#equivalentProperty",query="SELECT DISTINCT ?prop ?equivprop ?label WHERE { ?prop %%equivprop%% ?equivprop . OPTIONAL { ?equivprop %%labelproperty%% ?label .} }"):
+        #QgsMessageLog.logMessage("Execute query: "+str(query), MESSAGE_CATEGORY, Qgis.Info)
+        query=query.replace("%%equivprop%%","<"+equivalentPropProperty+">").replace("%%labelproperty%%","<http://www.w3.org/2000/01/rdf-schema#label>")
+        results=SPARQLUtils.executeQuery(triplestoreurl,query,{"auth":{"method":authmethod,"userCredential":credentialUserName,"userPassword":credentialPassword}})
+        #QgsMessageLog.logMessage("Query results: "+str(results), MESSAGE_CATEGORY, Qgis.Info)
+        res={}
+        if results!=False:
+            for restup in results:
+                if str(restup["prop"]) not in res:
+                    res[str(restup["prop"])]=[]
+                res[str(restup["prop"])].append({"uri":restup["equivprop"]})
+            return res
+        return res
+
+    def detectEquivalentClasses(self,triplestoreurl,credentialUserName,credentialPassword, authmethod,equivalentClassProperty="http://www.w3.org/2002/07/owl#equivalentClass",query="SELECT DISTINCT ?cls ?equivcls ?label WHERE { { ?cls <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> owl:Class } UNION { ?ind <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> ?cls } ?cls %%equivprop%% ?equivcls . OPTIONAL { ?equivcls %%labelproperty%% ?label .} }"):
+        #QgsMessageLog.logMessage("Execute query: "+str(query), MESSAGE_CATEGORY, Qgis.Info)
+        query=query.replace("%%equivcls%%","<"+equivalentClassProperty+">").replace("%%labelproperty%%","<http://www.w3.org/2000/01/rdf-schema#label>")
+        results=SPARQLUtils.executeQuery(triplestoreurl,query,{"auth":{"method":authmethod,"userCredential":credentialUserName,"userPassword":credentialPassword}})
+        #QgsMessageLog.logMessage("Query results: "+str(results), MESSAGE_CATEGORY, Qgis.Info)
+        res={}
+        if results!=False:
+            for restup in results:
+                if str(restup["cls"]) not in res:
+                    res[str(restup["cls"])]=[]
+                res[str(restup["cls"])].append({"uri":restup["equivcls"]})
+            return res
+        return res
+
     def detectGeometryLiteralRelations(self,configuration,credentialUserName,credentialPassword, authmethod):
         newrels=[]
         results=self.testTripleStoreConnection(configuration["resource"], self.testQueries["discoverLiteralRels"],
