9f24fa0ed47b177e71ced119deea2fd56135d6de
Timo <situx@users.noreply.github.com>
2023-03-22 16:29:58 +0100

    rdfa fixes

--- util/doc/ontdocgeneration.py
+++ util/doc/ontdocgeneration.py
@@ -100,7 +100,7 @@ classtreequery="""PREFIX owl: <http://www.w3.org/2002/07/owl#>\n
         PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n
         SELECT DISTINCT ?subject ?label ?supertype\n
         WHERE {\n
-           { ?individual rdf:type ?subject . } UNION { ?subject rdf:type owl:Class . } .\n
+           { ?individual rdf:type ?subject . } UNION { ?subject rdf:type owl:Class . } UNION { ?subject rdf:type rdfs:Class . }.\n
            OPTIONAL { ?subject rdfs:subClassOf ?supertype } .\n
            OPTIONAL { ?subject rdfs:label ?label. filter(langMatches(lang(?label),\"en\")) }
            OPTIONAL { ?subject rdfs:label ?label }.\n
@@ -808,7 +808,7 @@ class OntDocGeneration:
             else:
                 if object.language != None:
                     tablecontents += "<span property=\"" + str(pred) + "\" content=\"" + str(
-                        object).replace("<", "&lt").replace(">", "&gt;").replace("\"","'") + "\" datatype=\"http://www.w3.org/2001/XMLSchema#string\" xml:lang=\"" + str(object.language) + "\">" + str(object).replace("<", "&lt").replace(">", "&gt;") + " <small>(<a style=\"color: #666;\" target=\"_blank\" href=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#langString\">rdf:langString</a>) (<a href=\"http://www.lexvo.org/page/iso639-1/"+str(object.language)+"\" target=\"_blank\">iso6391:" + str(object.language) + "</a>)</small></span>"
+                        object).replace("<", "&lt").replace(">", "&gt;").replace("\"","'") + "\" datatype=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#langString\" xml:lang=\"" + str(object.language) + "\">" + str(object).replace("<", "&lt").replace(">", "&gt;") + " <small>(<a style=\"color: #666;\" target=\"_blank\" href=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#langString\">rdf:langString</a>) (<a href=\"http://www.lexvo.org/page/iso639-1/"+str(object.language)+"\" target=\"_blank\">iso6391:" + str(object.language) + "</a>)</small></span>"
                 else:
                     tablecontents += self.detectStringLiteralContent(pred,object)
         return {"html":tablecontents,"geojson":geojsonrep,"foundmedia":foundmedia,"imageannos":imageannos,"textannos":textannos,"image3dannos":image3dannos,"label":label}
