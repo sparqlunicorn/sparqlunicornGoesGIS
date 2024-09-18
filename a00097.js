26e438f86cdcc43930973e557176bfc6821566bf
situx <situx@users.noreply.github.com>
2024-09-18 13:37:14 +0000

    Deploying to gh-pages from  @ a738561534a5fad73820538cb7d5125d9d587048 🚀

--- a00097.js
+++ a00097.js
@@ -4,7 +4,7 @@ var a00097 =
     [ "detectLayerColumnType", "a00097.html#a7719daa9b701fee94681d6d677675cef", null ],
     [ "detectLayerColumnTypes", "a00097.html#af428e0da9890c28e8471632877684873", null ],
     [ "exportGeometryType", "a00097.html#a6ab4400bb1d621394fec9af6ed12467b", null ],
-    [ "findColumnNameProperties", "a00097.html#ac397140c9607fe5e09970dde7a9f945c", null ],
+    [ "findColumnNameProperties", "a00097.html#a181b3aec1c36ac8db153cd85b0050279", null ],
     [ "getLayerColumnAsList", "a00097.html#aa1a88978ad30c85e5b095f9d079d1547", null ],
     [ "processLiteral", "a00097.html#a40028de6fa936f8dd159da67ea1734be", null ],
     [ "reprojectGeometry", "a00097.html#ab0b775b208048ccbfc14034c52dac205", null ],
