6d112b587bb6d84ae54806ba5f016c321eecbe96
Timo <situx@users.noreply.github.com>
2025-02-03 14:32:49 +0100

    added cache expiry time

--- tasks/processing/loadtriplestoretask.py
+++ tasks/processing/loadtriplestoretask.py
@@ -2,10 +2,13 @@ from ...util.sparqlutils import SPARQLUtils
 from rdflib import Graph
 from qgis.core import Qgis,QgsTask, QgsMessageLog
 import os
+import time
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 MESSAGE_CATEGORY = 'LoadTripleStoreTask'
+
+CACHEDAYS=30
 
 ## Loads a graph from an RDF file either by providing an internet address or a file path.
 class LoadTripleStoreTask(QgsTask):
@@ -16,6 +19,11 @@ class LoadTripleStoreTask(QgsTask):
         self.endpointIndex=endpointIndex
         self.curtriplestoreconf = curtriplestoreconf
         self.dlg=dlg
+
+    @staticmethod
+    def is_file_older_than_x_days(path, days=1):
+        modtime=os.path.getmtime(path)
+        return ((time.time() - modtime) / 3600 > 24 * days)
 
     def run(self):
         #QgsMessageLog.logMessage('Started task: Serializing loaded graph to '.format(self.description()) + str(self.curtriplestoreconf["resource"]["url"]),
@@ -28,11 +36,12 @@ class LoadTripleStoreTask(QgsTask):
             path = os.path.join(__location__, "../../tmp/graphcache/" + str(
                 str(self.curtriplestoreconf["resource"]["url"]).replace("/", "_").replace("['", "")
                 .replace("']","").replace("\\", "_").replace(":", "_")) + ".ttl")
-            if os.path.isfile(path):
-                #QgsMessageLog.logMessage('Started task: Loading graph from file '.format(self.description()) + str(path),MESSAGE_CATEGORY, Qgis.Info)
+            if os.path.isfile(path) and not LoadTripleStoreTask.is_file_older_than_x_days(path,CACHEDAYS):
+                QgsMessageLog.logMessage('FILE IS PRESENT AND NOT OLDER THAN '+str(CACHEDAYS),MESSAGE_CATEGORY, Qgis.Info)
                 self.graph.parse(path)
                 self.curtriplestoreconf["instance"]=self.graph
             else:
+                QgsMessageLog.logMessage('FILE IS NOT PRESENT OR OLDER THAN '+str(CACHEDAYS),MESSAGE_CATEGORY, Qgis.Info)
                 #QgsMessageLog.logMessage('Started task: Loading graph from URI '.format(self.description()) +self.curtriplestoreconf["resource"]["url"],
                 #                         MESSAGE_CATEGORY, Qgis.Info)
                 SPARQLUtils.loadGraph(self.curtriplestoreconf["resource"]["url"],self.graph)
